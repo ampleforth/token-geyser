8e3655bd87145b8187dd758202c742feac273b6d
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-04 21:08:48 +0800

    Delete .solcover.js

--- .solcover.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  norpc: true,
-  testCommand: 'npm test',
-  compileCommand: 'npm run compile-contracts',
-  copyPackages: ['openzeppelin-eth', 'openzeppelin-solidity', 'uFragments'],
-  skipFiles: ['IStaking.sol'],
-};
